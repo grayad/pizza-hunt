48fdd467dc6098778fb857d5937efb5c01b098e4
Alexus Gray <alexus_gray@alumni.unc.edu>
2022-08-11 14:48:14 -0400

    set up comment controller with create and remove comment functionality

---
+++ controllers/comment-controller.js
@@ -0,0 +1,55 @@
+// import models needed
+const { Pizza, Comment } = require("../models");
+
+// create commentController object
+const commentController = {
+  // add comment to pizza
+  addComment({ params, body }, res) {
+    console.log(body);
+    Comment.create(body)
+      .then(({ _id }) => {
+        return Pizza.findOneAndUpdate(
+          { _id: params.pizzaId },
+          //   use $push method to add the comment's id to the specific pizza
+          { $push: { comments: _id } },
+          //   receive back the updated pizza with the new comment instead of the current version
+          { new: true }
+        );
+      })
+      .then((dbPizzaData) => {
+        if (!dbPizzaData) {
+          res.status(404).json({ message: "No pizza found with this id!" });
+          return;
+        }
+        res.json(dbPizzaData);
+      })
+      .catch((err) => res.json(err));
+  },
+  // remove comment
+  removeComment({ params }, res) {
+    // delete a comment while also returning its data
+    Comment.findOneAndDelete({ _id: params.commentId })
+      .then((deletedComment) => {
+        if (!deletedComment) {
+          return res.status(404).json({ message: "No comment with this id!" });
+        }
+        // use comment data to identifiy and remove it from the associated pizza
+        return Pizza.findOneAndUpdate(
+          { _id: params.pizzaId },
+          //   $pull is the mongo operation to remove the comment
+          { $pull: { comments: params.commentId } },
+          { new: true }
+        );
+      })
+      .then((dbPizzaData) => {
+        if (!dbPizzaData) {
+          res.status(404).json({ message: "No pizza found with this id!" });
+          return;
+        }
+        res.json(dbPizzaData);
+      })
+      .catch((err) => res.json(err));
+  },
+};
+
+module.exports = commentController;
